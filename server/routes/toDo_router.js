f199c947b5be22da3f62291ceae3151ac6d44732
jmbielejeski <jmbielejeski@gmail.com>
2021-02-13 10:38:46 -0700

    GET route is working

--- server/routes/toDo_router.js
+++ server/routes/toDo_router.js
@@ -2,4 +2,31 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+// GET the current database
+router.get('/', (req, res) => {
+  console.log('in GET route');
+  // pool is our DB connection
+  pool
+    .query(
+      `
+    -- selecting entire table
+    SELECT * FROM "todo"
+    ORDER BY "id" -- ordering by ID
+  `
+    )
+    .then(function (dbRes) {
+      console.log('dbRes rows is ', dbRes.rows);
+      // send back DB results
+      res.send(dbRes.rows);
+    })
+    .catch(function (error) {
+      console.log('error getting DB table', error);
+      res.sendStatus(500);
+    });
+});
+
+// POST
+
+// DELETE
+
 module.exports = router;
